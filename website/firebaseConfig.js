5735d6dc522497892537e00dff5f2921bad0ab0d
김덕배 <97142351+Kim-Dukbae@users.noreply.github.com>
2025-04-21 20:50:40 +0900

    Update firebaseConfig.js

--- website/firebaseConfig.js
+++ website/firebaseConfig.js
@@ -8,3 +8,5 @@ const firebaseConfig = {
   messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
   appId: "YOUR_APP_ID"
 };
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore(); // 또는 realtime database라면 firebase.database();
